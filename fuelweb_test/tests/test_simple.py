1f61ddeedd8d70b901f7667e8ed082c2b9c9cae8
Nikita Prikazchikov <nprikazchikov@mirantis.com>
2013-10-29 15:46:20 +0400

    Done test simple vlan
    Change-Id: I4badae571550135972bc69c87d38edd53a800654

--- fuelweb_test/tests/test_simple.py
+++ fuelweb_test/tests/test_simple.py
@@ -24,9 +24,31 @@ logwrap = debug(logger)
 
 
 @test
+class OneNodeDeploy(TestBasic):
+
+    @log_snapshot_on_error
+    @test(groups=["thread_2"], depends_on=[TestBasic.prepare_release])
+    def deploy_one_node(self):
+        self.env.revert_snapshot("ready")
+        self.fuel_web.client.get_root()
+        self.env.bootstrap_nodes(self.env.nodes()[:1])
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller']}
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        self.fuel_web.assert_cluster_ready(
+            'slave-01', smiles_count=4, networks_count=1, timeout=300)
+
+
+@test
 class SimpleFlat(TestBasic):
     @log_snapshot_on_error
-    @test(groups=["thread_1"], depends_on=[TestBasic.prepare_slaves])
+    @test(groups=["thread_2"], depends_on=[TestBasic.prepare_slaves])
     def deploy_simple_flat(self):
         self.env.revert_snapshot("ready_with_3_slaves")
 
@@ -47,7 +69,7 @@ class SimpleFlat(TestBasic):
         self.env.make_snapshot("deploy_simple_flat")
 
     @log_snapshot_on_error
-    @test(groups=["thread_1"], depends_on=[deploy_simple_flat])
+    @test(groups=["thread_2"], depends_on=[deploy_simple_flat])
     def simple_flat_verify_networks(self):
         self.env.revert_snapshot("deploy_simple_flat")
 
@@ -58,9 +80,54 @@ class SimpleFlat(TestBasic):
         self.fuel_web.assert_task_success(task, 60 * 2, interval=10)
 
     @log_snapshot_on_error
-    @test(groups=["thread_1"], depends_on=[deploy_simple_flat])
+    @test(groups=["thread_2"], depends_on=[deploy_simple_flat])
     def simple_flat_ostf(self):
         self.env.revert_snapshot("deploy_simple_flat")
+
+        self.fuel_web.run_ostf(
+            cluster_id=self.fuel_web.get_last_created_cluster(),
+            should_fail=5, should_pass=19
+        )
+
+
+@test
+class SimpleVlan(TestBasic):
+    @log_snapshot_on_error
+    @test(groups=["thread_2"], depends_on=[TestBasic.prepare_slaves])
+    def deploy_simple_vlan(self):
+        self.env.revert_snapshot("ready_with_3_slaves")
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_SIMPLE
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute']
+            }
+        )
+        self.fuel_web.update_vlan_network_fixed(
+            cluster_id, amount=8, network_size=32)
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        self.fuel_web.assert_cluster_ready(
+            'slave-01', smiles_count=6, networks_count=1, timeout=300)
+        self.env.make_snapshot("deploy_simple_vlan")
+
+    @log_snapshot_on_error
+    @test(groups=["thread_2"], depends_on=[deploy_simple_vlan])
+    def simple_flat_verify_networks(self):
+        self.env.revert_snapshot("deploy_simple_vlan")
+
+        task = self.fuel_web.run_network_verify(
+            self.fuel_web.get_last_created_cluster())
+        self.fuel_web.assert_task_success(task, 60 * 2, interval=10)
+
+    @log_snapshot_on_error
+    @test(groups=["thread_2"], depends_on=[deploy_simple_vlan])
+    def simple_flat_ostf(self):
+        self.env.revert_snapshot("deploy_simple_vlan")
 
         self.fuel_web.run_ostf(
             cluster_id=self.fuel_web.get_last_created_cluster(),
